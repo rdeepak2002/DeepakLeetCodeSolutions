82d201ec8a7ecacc412a0d7f4cff8ca882c196ca
rdeepak2002 <rdeepak2002@gmail.com>
2020-03-18 17:08:34 -0700

    finished regex

--- src/Main.java
+++ src/Main.java
@@ -491,8 +491,37 @@ public class Main {
      * s could be empty and contains only lowercase letters a-z.
      * p could be empty and contains only lowercase letters a-z, and characters like . or *.
      */
-    // My solution:
+    // Best solution (https://youtu.be/l3hda49XcDE):
     public boolean isMatch(String s, String p) {
-        
+        char[] text = s.toCharArray();
+        char[] pattern = p.toCharArray();
+        boolean[][] T = new boolean[text.length+1][pattern.length+1];
+
+        T[0][0] = true;
+
+        for(int i = 1; i < T[0].length; i++) {
+            if(pattern[i-1] == '*') {
+                T[0][i] = T[0][i-2];
+            }
+        }
+
+        for(int i = 1; i < T.length; i++) {
+            for(int j = 1; j < T[i].length; j++) {
+                if(pattern[j-1] == '.' || pattern[j-1] == text[i-1]) {
+                    T[i][j] = T[i-1][j-1];
+                }
+                else if(pattern[j-1] == '*') {
+                    T[i][j] = T[i][j-2];
+                    if(pattern[j-2] == '.' || pattern[j-2] == text[i-1]) {
+                        T[i][j] = T[i][j] || T[i-1][j];
+                    }
+                }
+                else {
+                    T[i][j] = false;
+                }
+            }
+        }
+
+        return T[text.length][pattern.length];
     }
 }
